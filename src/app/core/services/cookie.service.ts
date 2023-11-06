ae181c5480adfdb2be1ee0420e5df33e4b49640a
klajdi.hasani <hasaniklajdi17@gmail.com>
2023-11-06 17:17:30 +0100

    Added the layout structure and some services

---
+++ src/app/core/services/cookie.service.ts
@@ -0,0 +1,10 @@
+import {Injectable, Optional, SkipSelf} from '@angular/core';
+import {CookieService as NGXCookieService} from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookieService {
+
+  constructor() { }
+}
---
+++ src/app/core/services/login.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {UserModel} from "../models/models";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  url = environment.API_HOST;
+
+  constructor(private fb: FormBuilder, private httpClient: HttpClient) { }
+
+
+  public signUp(user: UserModel): Observable<any> {
+    return this.httpClient.post(this.url + '/register', user, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
+
+  public login(data: any) {
+    return this.httpClient.post(this.url + '/login', data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
+
+  initLoginFG(): FormGroup {
+    return this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+}
+
+
