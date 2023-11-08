648321d492ac3aa6f7a0ad2fdb3f64c0a61a6498
klajdi.hasani <hasaniklajdi17@gmail.com>
2023-11-08 09:49:57 +0100

    Created Banking Component module

---
+++ src/app/banking-component/banking-component.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BankingComponentRoutingModule } from './banking-component-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BankingComponentRoutingModule
+  ]
+})
+export class BankingComponentModule { }
